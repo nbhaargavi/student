4af2d9ec51b51dbcc6a1f62395cefc94f0b3fb84
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-01-26 22:19:18 +0530

    Update student.java

--- src/main/java/org/example/student.java
+++ src/main/java/org/example/student.java
@@ -5,12 +5,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class student {
+public class Student {
     String name;
     int gradelvl;
     int gpa;
 
-    public student(String name,int gradelvl,int gpa)
+    public Student(String name,int gradelvl,int gpa)
     {
         this.name=name;
         this.gradelvl=gradelvl;
@@ -25,7 +25,7 @@ public class student {
     {
         System.out.println(name+ " has a gpa of "+gpa);
     }
-    public static void main(String args[]) throws IOException
+    public static void main(String[] args) throws IOException
     {
 
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
@@ -35,7 +35,7 @@ public class student {
         int gpa=Integer.parseInt(br.readLine());
         System.out.println("Enter student's updated gpa");
         int ugpa=Integer.parseInt(br.readLine());
-        student s1= new student(str,gpa,ugpa);
+        Student s1= new Student(str,gpa,ugpa);
         s1.update(ugpa);
         s1.stud();
     }
