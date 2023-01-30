dad036c2926397009332f724f6699a4f8b556cb4
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-01-30 16:56:31 +0530

    Update student.java

--- src/main/java/org/example/student.java
+++ src/main/java/org/example/student.java
@@ -1,17 +1,14 @@
-package org.example;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class student {
+public class Student {
     Logger l=Logger.getLogger("kitty");
     String name;
     int gradelvl;
     int gpa;
-    public student(String name,int gradelvl,int gpa)
+    public Student(String name,int gradelvl,int gpa)
     {
         this.name=name;
         this.gradelvl=gradelvl;
@@ -20,11 +17,12 @@ public class student {
     void update(int update)
     {
         gpa=update;
-        System.out.println("The updated GPA is "+gpa);
+        l.log(Level.INFO,() ->"The updated GPA is "+gpa);
     }
     void stud()
     {
-        System.out.println(name+ " has a gpa of "+gpa);
+
+        l.log(Level.INFO,() ->name+ " has a gpa of "+gpa);
     }
     public static void main(String args[]) throws IOException
     {
@@ -36,8 +34,9 @@ public class student {
         int gpa=Integer.parseInt(br.readLine());
         l.info("Enter student's updated gpa");
         int ugpa=Integer.parseInt(br.readLine());
-        student s1= new student(str,gpa,ugpa);
+        Student s1= new Student(str,gpa,ugpa);
         s1.update(ugpa);
         s1.stud();
     }
 }
+
